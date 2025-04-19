86a24bf3f2a78f8af7923aabd4431615da4efd26
BlueSugar <emerald620blue@gmail.com>
2025-04-19 19:02:50 +0900

    abc400

--- abc400/src/bin/f.rs
+++ abc400/src/bin/f.rs
@@ -12,6 +12,43 @@ type Deque<T> = VecDeque<T>;
 const MOD: u64 = 998_244_353;
 
 fn main() {
-    input!{
+    input! {
+        n: usize,
+        c: [Usize1; n],
+        x: [usize; n],
     }
+
+    let mut dp = vec![vec![!0 / 2; 2 * n + 1]; 2 * n + 1];
+    for i in 0..2 * n {
+        dp[i][i + 1] = x[c[i % n]] + 1;
+    }
+
+    let mut ep = vec![vec![!0 / 2; 2 * n + 1]; 2 * n + 1];
+    for i in 0..2 * n {
+        ep[i][i + 1] = 0;
+    }
+
+    for d in 2..=n {
+        for l in 0..2 * n - d {
+            let r = l + d;
+            for o in l + 1..r {
+                dp[l][r] = dp[l][r].min(dp[l][o] + dp[o][r]);
+            }
+            for o in l + 1..r {
+                ep[l][r] = ep[l][r].min(ep[l][o] + dp[o][r]);
+            }
+            if c[l % n] == c[(r - 1) % n] {
+                ep[l][r] = ep[l][r].min(ep[l][r - 1]);
+            };
+            if c[l % n] == c[(r - 1) % n] {
+                dp[l][r] = dp[l][r].min(ep[l][r] + r - l + x[c[l % n]]);
+            }
+        }
+    }
+
+    let mut ans = !0;
+    for i in 0..n {
+        ans = ans.min(dp[i][i + n]);
+    }
+    println!("{}", ans);
 }
--- abc400/src/bin/g.rs
+++ abc400/src/bin/g.rs
@@ -10,8 +10,68 @@ type Set<T> = BTreeSet<T>;
 type Deque<T> = VecDeque<T>;
 
 const MOD: u64 = 998_244_353;
+const INF: i64 = 1 << 50;
 
 fn main() {
-    input!{
+    input! {
+        t: usize,
+    }
+
+    for _ in 0..t {
+        input! {
+            n: usize,
+            k: usize,
+            mut xyz: [[i64; 3]; n],
+        }
+
+        xyz.sort_unstable_by_key(|xyz| *xyz.iter().max().unwrap());
+        xyz.reverse();
+
+        // dp[i][t][b]: [0, i) のなかでスルーしたのが t 個で現在の選択の偶奇状況が b
+        // であるようなものの最大値
+        let mut dp = [[-INF; 8]; 3];
+        dp[0][0] = 0;
+
+        for xyz in xyz[..2 * k].iter() {
+            let mut ep = [[-INF; 8]; 3];
+            // k として選択する。
+            for k in 0..3 {
+                for t in 0..=2 {
+                    for b in 0..8 {
+                        ep[t][b ^ (1 << k)] = ep[t][b ^ (1 << k)].max(dp[t][b] + xyz[k]);
+                    }
+                }
+            }
+            // 何も選択せずにスルーする。
+            for t in 0..2 {
+                for b in 0..8 {
+                    ep[t + 1][b] = ep[t + 1][b].max(dp[t][b]);
+                }
+            }
+
+            dp = ep;
+        }
+
+        for xyz in xyz[2 * k..].iter() {
+            let mut ep = [[-INF; 8]; 3];
+            // 何も選択せずにスルーする。
+            for t in 0..=2 {
+                for b in 0..8 {
+                    ep[t][b] = ep[t][b].max(dp[t][b]);
+                }
+            }
+            // k として選択する。
+            for k in 0..3 {
+                for t in 1..=2 {
+                    for b in 0..8 {
+                        ep[t - 1][b ^ (1 << k)] = ep[t - 1][b ^ (1 << k)].max(dp[t][b] + xyz[k]);
+                    }
+                }
+            }
+
+            dp = ep;
+        }
+
+        println!("{}", dp[0][0]);
     }
 }
