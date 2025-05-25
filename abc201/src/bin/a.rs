1c271ba3801735ff3dca5a7cfc1e4f57c3538e6a
BlueSugar <emerald620blue@gmail.com>
2025-05-25 22:05:49 +0900

    udpate

--- abc201/src/bin/a.rs
+++ abc201/src/bin/a.rs
@@ -1,23 +1,4 @@
-use itertools::*;
-use memoise::*;
-use num::integer::{nth_root, sqrt};
-use proconio::marker::*;
-use proconio::*;
-use rand::{thread_rng, Rng};
-use std::collections::*;
-use superslice::Ext;
-
-type Map<K, V> = BTreeMap<K, V>;
-type Set<T> = BTreeSet<T>;
-type Deque<T> = VecDeque<T>;
-type Heap<T> = BinaryHeap<T>;
-
-const MOD: u64 = 998_244_353;
-
 fn main() {
-    input! {
-        mut a: [u8; 3],
+    input!{
     }
-    a.sort_unstable();
-    println!("{}", if a[0] + a[2] == 2 * a[1] { "Yes" } else { "No" });
 }
--- abc201/src/bin/f.rs
+++ abc201/src/bin/f.rs
@@ -1,20 +1,3 @@
-use itertools::*;
-use memoise::*;
-use num::integer::{nth_root, sqrt};
-use proconio::marker::*;
-use proconio::*;
-use rand::{thread_rng, Rng};
-use std::collections::*;
-use superslice::Ext;
-
-type Map<K, V> = BTreeMap<K, V>;
-type Set<T> = BTreeSet<T>;
-type Deque<T> = VecDeque<T>;
-type Heap<T> = BinaryHeap<T>;
-
-const MOD: u64 = 998_244_353;
-
-
 fn main() {
     input!{
     }
